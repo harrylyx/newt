a3d49ed4e7b9b19d093d6bfda65edfbe6a0c5ddb
harrylyx <lyxvae@gmail.com>
2026-04-04 23:12:43 +0800

    chore(release): bump version to 0.1.3

--- src/newt/__init__.py
+++ src/newt/__init__.py
@@ -10,7 +10,7 @@ A comprehensive Python library for building credit scorecards with:
 - Pipeline-style workflow
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # WOE analysis
 from newt.features.analysis import WOEEncoder
